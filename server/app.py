e778836872ee0c3bfb54d1452df520815712bbd1
paunchshaker <ernfrid@gmail.com>
2019-12-22 21:59:04 -0600

    Add support for updating books

--- server/app.py
+++ server/app.py
@@ -57,6 +57,31 @@ def all_books():
         response_object['books'] = BOOKS
     return jsonify(response_object)
 
+@app.route('/books/<book_id>', methods=['PUT'])
+def single_book(book_id):
+    response_object = {'status': 'success'}
+    if request.method == 'PUT':
+        post_data = request.get_json()
+        book = find_book(book_id)
+        book['title'] = post_data.get('title')
+        book['author'] = post_data.get('author')
+        book['read'] = post_data.get('read')
+        response_object['message'] = 'Book updated!'
+    return jsonify(response_object)
+
+def find_book(book_id):
+    for book in BOOKS:
+        if book['id'] == book_id:
+            return book
+    return None
+
+def remove_book(book_id):
+    for book in BOOKS:
+        if book['id'] == book_id:
+            BOOKS.remove(book)
+            return True
+    return False
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
