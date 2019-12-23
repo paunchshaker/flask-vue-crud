894816438bbd1401c31b79b741b5d4dde294b3f4
paunchshaker <ernfrid@gmail.com>
2019-12-22 21:51:43 -0600

    Assign uuid

--- server/app.py
+++ server/app.py
@@ -1,3 +1,5 @@
+import uuid
+
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
@@ -7,16 +9,19 @@ DEBUG = True
 
 BOOKS = [
     {
+        'id':  uuid.uuid4().hex,
         'title': 'The Three-Body Problem',
         'author': 'Cinxin Liu',
         'read': True
     },
     {
+        'id':  uuid.uuid4().hex,
         'title': '1984',
         'author': 'George Orwell',
         'read': True
     },
     {
+        'id':  uuid.uuid4().hex,
         'title': 'Moby Dick',
         'author': 'Herman Melville',
         'read': False
@@ -42,6 +47,7 @@ def all_books():
     if request.method == 'POST':
         post_data = request.get_json()
         BOOKS.append({
+            'id':  uuid.uuid4().hex,
             'title': post_data.get('title'),
             'author': post_data.get('author'),
             'read': post_data.get('read')
